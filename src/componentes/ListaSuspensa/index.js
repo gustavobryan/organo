6f3493cf23dbd0a5ecd22f36dff22126130b6025
Gustavo Bryan <devgustavob@gmail.com>
2023-04-16 21:49:21 -0300

    componente lista suspensa

---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,18 @@
+import './ListaSuspensa'
+
+const ListaSuspensa = (props) => {
+    console.log(props.itens)
+
+    return (
+        <div>
+            <label>{props.label}</label>
+            <select>
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                })}
+            </select>
+        </div>
+    )
+}
+
+export default ListaSuspensa
