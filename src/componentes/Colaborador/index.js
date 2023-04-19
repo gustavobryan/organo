53b999c31855e3f8cc4ea750101b47fe43baa454
Gustavo Bryan <devgustavob@gmail.com>
2023-04-19 16:03:53 -0300

    card colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src="https://github.com/gustavobryan.png" alt='foto colaborador'/>
+            </div>
+            <div className='rodape'>
+                <h4>Gustavo Bryan</h4>
+                <h5>Front-End</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
